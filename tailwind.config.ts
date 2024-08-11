abd1cc5387f578ede18e350d6a90819a7095f3f3
soumajit <soumojitghosh02@gmail.com>
2024-08-11 10:34:25 +0530

    ui modified

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,30 @@
-import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import {type Config} from "tailwindcss";
+import {fontFamily} from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+        mono: ["var(--font-mono)", ...fontFamily.mono],
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      {
+        myTheme: {
+          "primary": "#b5f18d",
+          "secondary": "#12917c",
+          "accent": "#44b7e8",
+          "neutral": "#244309",
+          "base-100": "#080f02",
+        },
+      },
+    ],
+  },
 } satisfies Config;
